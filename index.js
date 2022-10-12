b5c5204f2217151974848777601bae3c33b78361
--global <--global>
2022-10-11 19:59:43 -0700

    fixed linting issues

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const {
-  Client,
-  Collection,
-  GatewayIntentBits,
+	Client,
+	Collection,
+	GatewayIntentBits,
 } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.DirectMessages,
-    GatewayIntentBits.GuildMessages,
-  ],
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.GuildMessages,
+	],
 });
 
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs
-  .readdirSync(commandsPath)
-  .filter((file) => file.endsWith('.js'));
+	.readdirSync(commandsPath)
+	.filter((file) => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  client.commands.set(command.data.name, command);
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	client.commands.set(command.data.name, command);
 }
 
 const eventsPath = path.join(__dirname, 'events');
@@ -36,7 +36,8 @@ for (const file of eventFiles) {
 	const event = require(filePath);
 	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args));
-	} else {
+	}
+	else {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
 }
